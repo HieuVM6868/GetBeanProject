eb47cb69cd3dadcf91c60f90c3639ad7d052ce86
adiTYAshah123jnj <128502448+adiTYAshah123jnj@users.noreply.github.com>
2024-05-31 09:12:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ PATH = os.path.abspath(os.path.dirname(__file__))
 os.chdir(PATH)
 
 # Set up GitHub repository details
-repository_name = 'Get-Pair-Extraordinaire'
+repository_name = 'GetBeanProject'
 branch_name = None
-AUTHOR_NAME = 'Prakash4844'
-AUTHOR_EMAIL = os.environ.get('GIT_EMAIL', '81550376+Prakash4844@users.noreply.github.com')
+AUTHOR_NAME = 'adiTYAshah123jnj'
+AUTHOR_EMAIL = os.environ.get('GIT_EMAIL', '128502448+adiTYAshah123jnj@users.noreply.github.com')
 
 # Get GITHUB_TOKEN from GitHub secrets
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
